74c5ba19b9cfdfe8494523981749705ec4d9e420
lanqiu1986 <lisuji1986@163.com>
2017-03-23 22:02:59 +0800

    using scope to write order by created_at to film's review

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -21,7 +21,7 @@ class FilmsController < ApplicationController
 
   def show
     @film = Film.find(params[:id])
-    @reviews = @film.reviews.order("created_at DESC")
+    @reviews = @film.reviews.recent
   end
 
   def edit
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :film
   validates :content, presence: true
-
+  scope :recent, -> {order("created_at DESC")}
 end
