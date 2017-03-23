371d2a9246d5531972cc7a07c4bb1dd30145af69
lanqiu1986 <lisuji1986@163.com>
2017-03-23 22:00:16 +0800

    add order by created_at to group's review

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -21,7 +21,7 @@ class FilmsController < ApplicationController
 
   def show
     @film = Film.find(params[:id])
-    @reviews = @film.reviews
+    @reviews = @film.reviews.order("created_at DESC")
   end
 
   def edit
